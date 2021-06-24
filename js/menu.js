a41f5e68e57ee933def5277f1373a7c4fafdea21
Renzo Dupont <renzo@renzodupont.com>
2021-06-24 03:43:32 -0300

    Added Projects page and icons

--- js/menu.js
+++ js/menu.js
@@ -1,14 +1,3 @@
-document.querySelector(".portfolio-link").onclick = (e) => {
-  $("html, body").animate(
-    {
-      scrollTop: $(".portfolio").offset().top,
-    },
-    800
-  );
-  e.preventDefault();
-  return false;
-};
-
 document.querySelector(".recommendations-link").onclick = (e) => {
   $("html, body").animate(
     {
