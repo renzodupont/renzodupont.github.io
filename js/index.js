3b67f9703cfec5cf02144941fc189ece6f8709fc
Renzo Dupont <renzo@renzodupont.com>
2021-06-23 22:44:55 -0300

    Intro video and previews

--- js/index.js
+++ js/index.js
@@ -1,3 +1,21 @@
-$(document).ready(function () {
-  $(".container").show(1500);
-});
+document.querySelector(".portfolio-link").onclick = (e) => {
+  $("html, body").animate(
+    {
+      scrollTop: $(".portfolio").offset().top,
+    },
+    800
+  );
+  e.preventDefault();
+  return false;
+};
+
+document.querySelector(".recommendations-link").onclick = (e) => {
+  $("html, body").animate(
+    {
+      scrollTop: $(".linkedin-recommendations").offset().top,
+    },
+    800
+  );
+  e.preventDefault();
+  return false;
+};
